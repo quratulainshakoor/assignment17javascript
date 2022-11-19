7df68ee98dd2d951f8823e4fd41bbd7aac761fdb
Qurat-ul-ain Shakoor <55014098+quratulainshakoor@users.noreply.github.com>
2022-11-19 13:29:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+////Question for loop
+
+// //Answer 1
+// for (var  i = 200; i > 0 ; i-=3){
+//      console.log(i);
+//   }
+
+
+
+///Answer2
+
+// let names = 0;
+// while (names < 10) {
+//   names++; 
+//   console.log('Hello world');
+// };
+
+
+//  //Answer 3
+//   let x= 1;
+//   let y = 0;
+//  while (x <= 10) {
+//   x++;
+//  if (x % 2 == 0){
+//   document.write(x + "<br/>")
+//  }
+
+//  }
+
+// //Answer 4
+// let i = 1;
+// let n = 5;
+// do{
+//   i++;
+//   console.log(i);
+// } while (i <= n);
+
+
+//Answer 5
+// function sum(a, b) {
+//   console.log(a + b);
+  
+// }
+// sum(2,6)
+
+// //Answer 6
+// function value(width, height) {
+//   area = width * height;
+//   return  area;
+// }
+
+// console.log(value(2,3))
+
+
+
+
+////..........nested loop........//
+
+
+//  let num = prompt("Enter a value");
+// for (let i = 1; i < 10; i++){
+//   let  total = i * num;
+
+//   console.log("`${num} * ${i} = ${total}`");
+
+//   for(j= 1; j < 10; j++){
+//     console.log(`${num} * ${i} = ${total}`)
+//   }
+  
+// }
+
+// for (let i=1; i<= 10; i++){
+//   console.log("......Table of " + i + "is");
+//   for(let j =1; j <= 10; j++){
+//     console.log(i + "*" + j + "=" + (i*j));
+//   }
+// }
